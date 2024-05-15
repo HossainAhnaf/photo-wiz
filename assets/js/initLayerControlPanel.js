099f2d1be2cacb928e7e3953f0922e092dc6e519
HossainAhnaf <itsbuldusmp@gmail.com>
2024-05-15 11:53:47 +0600

    b

--- assets/js/initLayerControlPanel.js
+++ assets/js/initLayerControlPanel.js
@@ -1,21 +1,40 @@
-var layerControlPanel,closeButton;
+import {isMobile} from "./app.js"
+var layerControlPanel = {
+    mobile:null,
+    desktop:null
+}
 
- 
 
 function hide() {
-    layerControlPanel.parentElement.classList.remove('active')
+    const layerControlPanelRef = isMobile ? layerControlPanel.mobile : layerControlPanel.desktop 
+    layerControlPanelRef.parentElement.classList.remove('active')
 }
 function show() {
-    layerControlPanel.parentElement.classList.add('active')
+    const layerControlPanelRef = isMobile ? layerControlPanel.mobile : layerControlPanel.desktop 
+    layerControlPanelRef.parentElement.classList.add('active')
 }
+function toggle({currentTarget}){
+    console.log(currentTarget);
+  if (currentTarget.classList.contains("active")){
+   currentTarget.classList.remove("active")
+   hide()
+  }else{
+    currentTarget.classList.add("active")
+    show()
+  }
 
+}
 export default function initLayerControlPanel() {
-    layerControlPanel = document.querySelector(".layer-control-panel")
-    closeButton = layerControlPanel.querySelector('.header > .close-button ')
-
+    layerControlPanel.mobile = document.querySelector(".layer-control-panel[data-mobile='true']")
+    layerControlPanel.desktop = document.querySelector(".layer-control-panel[data-mobile='false']")
+   
+    const showFullContainerButton = layerControlPanel.desktop.parentElement.querySelector(".show-full-container-button")
+   const closeButton = layerControlPanel.mobile.querySelector('.header > .close-button ')
+   
+    showFullContainerButton.addEventListener('click', toggle)
     closeButton.addEventListener('click', hide)
 
     return {
-        ref: layerControlPanel, hide, show
+        layerControlPanel: layerControlPanel, hide, show
     }
 }
